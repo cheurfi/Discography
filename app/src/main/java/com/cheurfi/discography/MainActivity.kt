0db7c9170ddc5e4056aa33b16e10cd60c5d867b1
cheurfi <samira.cheurfi@gmail.com>
2023-03-05 15:45:46 +0000

    pass id to function, add switch between screens

--- app/src/main/java/com/cheurfi/discography/MainActivity.kt
+++ app/src/main/java/com/cheurfi/discography/MainActivity.kt
@@ -2,19 +2,15 @@ package com.cheurfi.discography
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.cheurfi.discography.data.Artist
+import com.cheurfi.discography.ui.composables.ArtistScreen
+import com.cheurfi.discography.ui.composables.SearchScreen
 import com.cheurfi.discography.ui.theme.DiscographyTheme
 import di.MyApplication
 import javax.inject.Inject
@@ -28,76 +24,47 @@ class MainActivity : ComponentActivity() {
         (applicationContext as MyApplication).appComponent.inject(this)
         super.onCreate(savedInstanceState)
         setContent {
-            DiscographyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    var text by remember { mutableStateOf("") }
-                    val artists by viewmodel.artists.collectAsState(emptyList())
-                    SearchScreen(text, onSearch = { viewmodel.getArtists(text) }, onValueChange = { text = it }, artists = artists)
+            var selectedArtist by remember { mutableStateOf<String?>(null) }
+
+            if (selectedArtist == null) {
+                SearchPage(viewmodel = viewmodel, onArtistSelected = { selectedArtist = it })
+            } else {
+                val artist =
+                ArtistPage(viewmodel = viewmodel, id = selectedArtist!!)
+                BackHandler {
+                    selectedArtist = null
                 }
             }
         }
     }
+
 }
 
 @Composable
-fun SearchScreen(
-    text: String,
-    onValueChange: (String) -> Unit,
-    onSearch: () -> Unit,
-    artists: List<Artist>,
-) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        Search(text, onValueChange, onSearch)
-        ArtistList(artists)
-    }
-}
+private fun SearchPage(viewmodel: DiscographyViewModel, onArtistSelected: (String) -> Unit) {
+    DiscographyTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
 
-@Composable
-fun Search(text: String, onValueChange: (String) -> Unit, onSearch: () -> Unit) {
-    TextField(
-        value = text,
-        label = { Text("Find artists") },
-        onValueChange = onValueChange,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(20.dp),
-        keyboardActions = KeyboardActions(onSearch = {
-            onSearch()
-        }),
-        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search)
-    )
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun ArtistList(artists: List<Artist>) {
-    LazyColumn(verticalArrangement = Arrangement.spacedBy(20.dp), modifier = Modifier.padding(20.dp)) {
-        items(artists) { artist ->
-            Card(
-                modifier = Modifier
-                    .fillMaxWidth()
-            ) {
-                Text(artist.name, modifier = Modifier.padding(20.dp))
-            }
+            val artists by viewmodel.artists.collectAsState(emptyList())
+            SearchScreen(
+                onSearch = { viewmodel.getArtists(it) },
+                artists = artists,
+                onArtistSelected = onArtistSelected
+            )
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+private fun ArtistPage(viewmodel: DiscographyViewModel, id: String) {
     DiscographyTheme {
-        Surface {
-            SearchScreen(
-                text = "Pharrel",
-                onValueChange = {},
-                onSearch = {},
-                artists = listOf(),
-            )
+        // A surface container using the 'background' color from the theme
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+
+            val artists by viewmodel.artists.collectAsState()
+            val artist = artists.first { id == it.id }
+            ArtistScreen(artist = artist)
         }
     }
 }
