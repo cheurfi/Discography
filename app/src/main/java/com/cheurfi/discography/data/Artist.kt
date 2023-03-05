c1266e179c359b8bad1562c48f8b49f32640d297
cheurfi <samira.cheurfi@gmail.com>
2023-03-05 15:44:56 +0000

    support nullable values

--- app/src/main/java/com/cheurfi/discography/data/Artist.kt
+++ app/src/main/java/com/cheurfi/discography/data/Artist.kt
@@ -3,21 +3,21 @@ package com.cheurfi.discography.data
 import com.google.gson.annotations.SerializedName
 
 data class Artist(
-    val aliases: List<Alias>,
-    val area: Area,
+    val aliases: List<Alias>?,
+    val area: Area?,
     @SerializedName("begin-area")
-    val beginArea: BeginArea,
-    val country: String,
-    val disambiguation: String,
+    val beginArea: BeginArea?,
+    val country: String?,
+    val disambiguation: String?,
     val id: String,
     @SerializedName("life-span")
-    val lifeSpan: LifeSpan,
+    val lifeSpan: LifeSpan?,
     val name: String,
-    val score: String,
+    val score: String?,
     @SerializedName("sort-name")
-    val sortName: String,
-    val tags: List<Tag>,
-    val type: String,
+    val sortName: String?,
+    val tags: List<Tag>?,
+    val type: String?,
 ) {
     data class Alias(
         @SerializedName("begin-date")
